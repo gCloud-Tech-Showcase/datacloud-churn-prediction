b763ec3b1efe359314d2a9d032a21ecd56289312
Carlos Hernandez <careduz@google.com>
2026-07-24 16:09:57 -0400

    Enable the Data Lineage API for BigQuery/Dataform lineage capture

--- infra/apis.tf
+++ infra/apis.tf
@@ -5,11 +5,12 @@
 
 resource "google_project_service" "apis" {
   for_each = toset([
-    "bigquery.googleapis.com",      # BQML training + queries
-    "aiplatform.googleapis.com",    # Vertex AI Model Registry (BQML model_registry='vertex_ai')
-    "dataform.googleapis.com",      # managed git-linked Dataform repository (anonymous, public repo)
-    "iam.googleapis.com",           # execution service account
-    "iamcredentials.googleapis.com" # service-account impersonation (Dataform act-as runner)
+    "bigquery.googleapis.com",       # BQML training + queries
+    "aiplatform.googleapis.com",     # Vertex AI Model Registry (BQML model_registry='vertex_ai')
+    "dataform.googleapis.com",       # managed git-linked Dataform repository (anonymous, public repo)
+    "iam.googleapis.com",            # execution service account
+    "iamcredentials.googleapis.com", # service-account impersonation (Dataform act-as runner)
+    "datalineage.googleapis.com"     # automatic BigQuery/Dataform data lineage capture
   ])
 
   service            = each.value
